b1c48235e723553d4f244d02fcd603d71b2f193b
Kristian Rekstad <krirek@msn.com>
2015-03-09 22:46:51 +0100

    Asd

--- core/src/com/polarbirds/darkness/DarknessGame.java
+++ core/src/com/polarbirds/darkness/DarknessGame.java
@@ -2,10 +2,9 @@ package com.polarbirds.darkness;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetLoaderParameters;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -20,6 +19,7 @@ public class DarknessGame extends Game {
     public SpriteBatch spriteBatch;
     public ModelBatch modelBatch;
     public final static AssetManager ASSET_MANAGER = new AssetManager();
+    public final static InputMultiplexer INPUT_MULTIPLEXER = new InputMultiplexer();
 
     Texture img;
 
@@ -33,25 +33,29 @@ public class DarknessGame extends Game {
 
         Bullet.init(false, true); // Init Bullet without refcount, and with logging
 
-        queueAssets(); // Load assets
+        Gdx.input.setInputProcessor(INPUT_MULTIPLEXER); // Set input handlers.
 
-        img = new Texture(Assets.texture.badlogic);
+        queueAssets(); // Load assets
 
         if (loadingScreen==null) loadingScreen = new LoadingScreen(this);
         if (gameScreen==null) gameScreen = new GameScreen(this);
 
         setScreen(loadingScreen);
+
+
+        // Set gfx settings
+        Gdx.gl.glEnable(GL30.GL_DEPTH_TEST);
+        Gdx.gl.glCullFace(GL30.GL_BACK);
     }
 
     @Override
     public void render() {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
+        //Gdx.gl.glDisable(GL30.GL_CULL_FACE); // dont cull sprites
 
-        spriteBatch.begin();
-        spriteBatch.draw(img, 0, 0, 50, 50);
-        spriteBatch.end();
+
 
         super.render();
     }
@@ -69,6 +73,7 @@ public class DarknessGame extends Game {
     private void queueAssets(){
         ASSET_MANAGER.load(Assets.model.weapon_teslaGun, Model.class);
         ASSET_MANAGER.load(Assets.model.debugEnemy, Model.class);
+        ASSET_MANAGER.load(Assets.model.map_T, Model.class);
     }
 
 }
